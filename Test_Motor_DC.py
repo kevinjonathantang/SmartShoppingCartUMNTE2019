0a19b9d2582758a3d7ffcb689caf076e00ed999b
Kevin Jonathan <96010922+kevinjonathantang@users.noreply.github.com>
2023-03-13 20:48:49 +0700

    Add files via upload

---
+++ Test_Motor_DC.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+
+in1 = 24
+in2 = 23
+en = 25
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(en, GPIO.OUT)
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+p = GPIO.PWM(en, 1000)
+p.start(25)
+
+while(1):
+    print("Testing")
+    GPIO.output(in1, GPIO.HIGH)
+    GPIO.output(in2, GPIO.LOW)
